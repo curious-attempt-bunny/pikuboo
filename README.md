40ead61fc9745ae84989cd85c9d524e6c9f962e4
curious-attempt-bunny <curious.attempt.bunny@gmail.com>
2021-03-07 17:43:29 -0800

    Working with the basics.

--- README.md
+++ README.md
@@ -1,8 +1,41 @@
+# Pikuboo
+
+Are your [piku](https://github.com/piku/piku) apps running on your raspberry pi(s)? Deploy `pikuboo` to a piku server to track the last heartbeat time for all the piku apps on your local network.
+
+# Deploy to piku
+
+```bash
+git remote add piku $PIKU_SERVER:pikuboo
+git push piku master
+```
+
+See [Using piku](https://github.com/piku/piku#using-piku).
+
+# Send a heartbeat
+
+```python
+import os.path
+def heartbeat(interval: float = 1.0, name: str = os.path.basename(os.path.dirname(os.path.realpath(__file__)))) -> None:
+    import socket
+    import time
+
+    name_bytes = bytes(name, 'utf-8')
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    
+    while True:
+        sock.sendto(name_bytes, ('255.255.255.255', 5555))
+        time.sleep(interval)
+
+import threading
+threading.Thread(target=heartbeat, daemon=True).start()
+```
+
+# Running locally
+
 ```
 python3 -m venv .venv
 source .venv/bin/activate
 python app.py
 ```
 
-* Listens on port 8080 for HTTP
-* Listens on port 5555 for UDP
--- app.py
+++ app.py
@@ -1,31 +1,34 @@
-import time
 import threading
-import dateutil.tz
+import time
 
 from flask import Flask
 app = Flask(__name__)
-
-timezone = dateutil.tz.gettz()
 
 heartbeat_status_lock = threading.Lock()
 heartbeat_status = {}
 
 @app.route('/')
 def status():
+    now =  time.time()
     with heartbeat_status_lock:
-        return heartbeat_status
-
+        return {
+            key:now-value
+            for key, value in heartbeat_status.items()
+        }
 
 def listen():
     import datetime
+    import dateutil.tz
     import socket
     import time
+
+    timezone = dateutil.tz.gettz()
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.bind(('0.0.0.0', 5555))
 
     print('Listening...')
-    while globals().get('__file__', None):
+    while True:
         try:
             data, addr = sock.recvfrom(1024, socket.MSG_DONTWAIT)
             data = data.decode('utf-8')
@@ -36,22 +39,21 @@ def listen():
         except BlockingIOError as e:
             time.sleep(0.05)
 
-    print('Done listening.')
-
-def heartbeat(interval: float) -> None:
+import os.path
+def heartbeat(interval: float = 1.0, name: str = os.path.basename(os.path.dirname(os.path.realpath(__file__)))) -> None:
     import socket
     import time
 
+    name_bytes = bytes(name, 'utf-8')
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
     
-    while globals().get('__file__', None):
-        sock.sendto(bytes(__file__, 'utf-8'), ('0.0.0.0', 5555))
+    while True:
+        sock.sendto(name_bytes, ('255.255.255.255', 5555))
         time.sleep(interval)
-    print('Done heartbeating.')
-
 
 if __name__ == '__main__':
-    threading.Thread(target=listen).start()
-    threading.Thread(target=heartbeat, args=[1]).start()
+    listen_thread = threading.Thread(target=listen, daemon=True).start()
+    threading.Thread(target=heartbeat, daemon=True).start()
 
     app.run(use_reloader=False, host='0.0.0.0', port=8080)
